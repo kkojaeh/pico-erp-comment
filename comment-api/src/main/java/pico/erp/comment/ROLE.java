ca62babab179628f90f656f0e307dd987968ceeb
Ethan <kkojaeh@gmail.com>
2018-09-11 11:29:24 +0900

    [Gradle Release Plugin] - new version commit:  '1.0.5'.

--- comment-api/src/main/java/pico/erp/comment/ROLE.java
+++ comment-api/src/main/java/pico/erp/comment/ROLE.java
@@ -1,26 +1,17 @@
 package pico.erp.comment;
 
-import java.util.Collections;
-import java.util.Set;
 import javax.persistence.Id;
-import javax.persistence.Transient;
 import lombok.Getter;
-import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
-import pico.erp.shared.data.Menu;
 import pico.erp.shared.data.Role;
 
 @RequiredArgsConstructor
 public enum ROLE implements Role {
 
-  COMMENT_MANAGER(Collections.emptySet());
+  COMMENT_MANAGER;
 
   @Id
   @Getter
   private final String id = name();
 
-  @Transient
-  @Getter
-  @NonNull
-  private Set<Menu> menus;
 }
